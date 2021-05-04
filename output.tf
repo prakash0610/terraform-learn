0b271b4649e3f91e818390f6d92ed293ccdb447c
Prakash Bhattarai <pabu696169@gmail.com>
2021-05-04 15:51:17 -0500

    add ec2 deployment

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
+output "myapp-vpc-id" {
+    value = aws_vpc.myapp-vpc.id
 }
 
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+output "myapp-subnet-id" {
+    value = aws_subnet.myapp-subnet-1.id
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "server_public_ip" {
+    value = aws_instance.myapp-server.public_ip
 }
